33e3a148a23a3e2e2938ca5603a3619ebef31a17
Ihor Sokolyk <ihorsokolyk1993@gmail.com>
2018-04-11 15:38:27 +0300

    Added reading from file task

--- src/com/test/Application.java
+++ src/com/test/Application.java
@@ -1,10 +1,16 @@
 package com.test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("First task result -> " + reverseString("Java test"));
         System.out.println("Second task result -> " + addNumbers(-23, 34));
+        readFile();
     }
 
     private static StringBuilder reverseString(String str) {
@@ -23,6 +29,29 @@ public class Application {
             b = c << 1;
         }
         return a;
+    }
+
+    private static void readFile() {
+        File file = new File("src/com/test/test.txt");
+        StringBuilder content = new StringBuilder();
+
+        try(FileInputStream fis = new FileInputStream(file)) {
+            int character;
+            while ((character = fis.read()) != -1) {
+                if (character == ',') {
+                    break;
+                } else {
+                    content.append((char)character);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Content before comma -> " + content);
+
     }
 
 
